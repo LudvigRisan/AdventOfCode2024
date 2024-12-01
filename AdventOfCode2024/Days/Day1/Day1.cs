46cc81e43be15fe8e2901e7a34eb64f93bcaecda
Ludvig Risan <ludvig.risan@gmail.com>
2024-12-01 21:25:28 +0100

    Init
    initial commit + day 1

---
+++ AdventOfCode2024/Days/Day1/Day1.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2024;
+
+public static class Day1
+{
+    public static string solve(){
+        StreamReader reader = new StreamReader("../../../inputs/Day1.txt");
+        string? line = reader.ReadLine();
+
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+        
+        while (line != null){
+            string[] numbers = line.Split(" ");
+            left.Add(int.Parse(numbers[0]));
+            right.Add(int.Parse(numbers[3]));
+            line = reader.ReadLine();
+        }
+        
+        left.Sort();
+        right.Sort();
+
+        int sum = 0;
+        for (int i = 0; i < left.Count; i++){
+            sum += left[i] * right.Count(x => x == left[i]);
+        }
+      
+        
+        return sum.ToString();
+    }
+}
