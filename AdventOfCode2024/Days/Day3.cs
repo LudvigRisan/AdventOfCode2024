03e54543fb4edd108964b87b27422284de4a8000
Ludvig Risan <ludvig.risan@gmail.com>
2024-12-04 20:40:23 +0100

    day 3

--- AdventOfCode2024/Days/Day3.cs
+++ AdventOfCode2024/Days/Day3.cs
@@ -4,20 +4,24 @@ namespace AdventOfCode2024;
 
 public static class Day3
 {
-    public static string solve()
-    {
+    public static string solve(){
         StreamReader reader = new StreamReader("../../../inputs/Day3.txt");
         string input = reader.ReadToEnd();
-        
-        Regex mulRegex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
-        
-        MatchCollection captures = mulRegex.Matches(input);
 
+        Regex doDontRegex = new Regex(@"do\(\)(?:(?!don't\(\)).)*|^(?:(?!don't\(\)).)*", RegexOptions.Singleline);
+        Regex mulRegex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+        MatchCollection dos = doDontRegex.Matches(input);
         int total = 0;
 
-        for (int i = 0; i < captures.Count; i++)
-        {
-            total += int.Parse(captures[i].Groups[1].Value) * int.Parse(captures[i].Groups[2].Value);
+        for (int j = 0; j < dos.Count; j++){
+            
+            MatchCollection captures = mulRegex.Matches(dos[j].Value);
+            
+            for (int i = 0; i < captures.Count; i++)
+            {
+                total += int.Parse(captures[i].Groups[1].Value) * int.Parse(captures[i].Groups[2].Value);
+            }
         }
         return total.ToString();
     }
