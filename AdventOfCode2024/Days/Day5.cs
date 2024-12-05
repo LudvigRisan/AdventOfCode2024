744155792d39412151c17e9bfedfbc2abbcb9c24
LudvigRisan <ludvig.risan@gmail.com>
2024-12-05 15:24:42 +0100

    Day5 init

---
+++ AdventOfCode2024/Days/Day5.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2024;
+
+public class Day5
+{
+    public static string solve()
+    {
+        StreamReader reader = new StreamReader("../../../inputs/Day5.txt");
+        string? line = reader.ReadLine();
+        
+        Dictionary<int, int> rules = new Dictionary<int, int>();
+        
+        
+        while (line != null)
+        {
+            
+            line = reader.ReadLine();
+        }
+
+        return "";
+    }
+}
