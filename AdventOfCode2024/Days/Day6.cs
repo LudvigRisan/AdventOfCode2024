04056bb0f2479c9b4cdadc30cfafac1b547d2575
LudvigRisan <ludvig.risan@gmail.com>
2024-12-06 14:00:07 +0100

    init day 6

---
+++ AdventOfCode2024/Days/Day6.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2024;
+
+public class Day6
+{
+    public static string solve()
+    {
+        StreamReader reader = new StreamReader("../../../inputs/Day6.txt");
+        string? line = reader.ReadLine();
+
+        while (line != null)
+        {
+            
+            
+            line = reader.ReadLine();
+        }
+        
+        return "";
+    }
+}
