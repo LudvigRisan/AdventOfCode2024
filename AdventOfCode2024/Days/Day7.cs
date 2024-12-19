42ec1cc49251f828ab92eec6ff400e22eb643538
Ludvig Temperli Risan <ludvig.risan@gmail.com>
2024-12-19 11:58:37 +0100

    Day7 finished

--- AdventOfCode2024/Days/Day7.cs
+++ AdventOfCode2024/Days/Day7.cs
@@ -2,10 +2,26 @@
 
 public class Day7
 {
-    static ulong opperate(ulong a, ulong b, bool multiply){
-        return multiply ? (a * b) : (a + b);
+    enum Operation
+    {
+        add = 0,
+        multiply = 1,
+        concat = 2
     }
-
+    static ulong opperate(ulong a, ulong b, Operation op){
+        switch (op)
+        {
+            case Operation.add:
+                return a + b;
+            case Operation.multiply:
+                return a * b;
+            case Operation.concat:
+                return ulong.Parse(a.ToString() + b.ToString());
+            default:
+                return 0;
+        }
+    }
+    
     static int intPow(int x, int pow){
         int ret = 1;
 
@@ -16,18 +32,35 @@ public class Day7
         return ret;
     }
     
-    static bool testSequence(ulong sum, ulong[] parts){
-        for (int i = 0; i < intPow(2, parts.Length - 1); i++){
-            ulong testSum = opperate(parts[0], parts[1], (i & 1) > 0);
-            for (int j = 1; j < parts.Length - 1; j++){
-                testSum = opperate(testSum, parts[j + 1], (i & intPow(2, j)) > 0);
-            }
+    static bool testSequence(ulong sum, ulong[] parts)
+    {
+        Operation[] ops = new Operation[parts.Length - 1];
+        for (int i = 0; i < ops.Length; i++)
+        {
+            ops[i] = Operation.add;
+        }
 
+        do
+        {
+            ulong testSum = opperate(parts[0], parts[1], ops[0]);
+            for (int j = 1; j < parts.Length - 1; j++){
+                testSum = opperate(testSum, parts[j + 1], ops[j]);
+            }
+            
             if (testSum == sum){
                 return true;
             }
+
+            for (int i = 0; i < ops.Length; i++)
+            {
+                ops[i] = (Operation)(((int)ops[i] + 1) % 3);
+                if (ops[i] != Operation.add)
+                {
+                    break;
+                }
+            }
         }
-        
+        while(ops.Any(x => x != Operation.add));
         return false;
     }
     
